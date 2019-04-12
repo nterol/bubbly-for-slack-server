ee0dd04c3d399358e2acaf5125b1c253ceb73dce
nterol <terolnicolas@gmail.com>
2019-04-12 13:56:14 +0200

    letssgo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const { createEventAdapter } = require("@slack/events-api");
+
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 const port = process.env.PORT || 9000;
@@ -8,6 +10,7 @@ const app = require("express")();
 app.use("/slack/events", slackEvents.expressMiddleware());
 
 slackEvents.on("message", event => {
+  console.log("EVENT", event);
   console.log(
     `Received a message: user ${event.user} in channel ${event.channel}`
   );
